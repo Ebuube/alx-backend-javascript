1c077c739896f52846dae3761450bc565c932af2
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-03 20:31:50 +0000

    Modified then statement

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    // eslint-disable-next-line arrow-body-style
-    .then(() => {
-      return { status: 200, body: 'Success' };
-    })
-    // eslint-disable-next-line arrow-body-style
-    .catch(() => {
-      return Error();
-    })
+    .then(() => (
+      { status: 200, body: 'Success' }
+    ))
+    .catch(() => (
+      Error()
+    ))
     .finally(() => {
       console.log('Got a response from the API');
     });
