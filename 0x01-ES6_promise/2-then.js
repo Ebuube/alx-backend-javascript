5691900dd74eff5680d900a21b60b1e250add785
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-03 20:23:17 +0000

    Returned promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then({ status: 200, body: 'Success' })
+  return promise
     // eslint-disable-next-line arrow-body-style
-    .catch((error) => { return Error(error); })
+    .then(() => {
+      return { status: 200, body: 'Success' };
+    })
+    // eslint-disable-next-line arrow-body-style
+    .catch((error) => {
+      return Error(error);
+    })
     .then(() => {
       console.log('Got a response from the API');
     });
