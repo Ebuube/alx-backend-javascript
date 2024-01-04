3500b4cc1203ebcd17bb1acdea3e32caa8b0b628
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-04 12:25:25 +0000

    Ensure empty is returned onfailure

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,17 +2,18 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function () {
   let result;
+  const empty = { photo: null, user: null };
 
   try {
     result = { photo: await uploadPhoto() };
   } catch (err) {
-    result = { photo: null };
+    return empty;
   }
 
   try {
     result = { ...result, user: await createUser() };
   } catch (err) {
-    result = { ...result, user: null };
+    return empty;
   }
 
   return result;
