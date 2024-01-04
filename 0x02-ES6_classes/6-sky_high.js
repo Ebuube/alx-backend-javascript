737165954702c547be0faebe27f59df6ab1e6ed5
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-04 18:41:09 +0000

    Task 6: Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(val) {
+    if (typeof val !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+
+    this._floors = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
