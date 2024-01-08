e5601c9bffb7969c0eba65bdfb8e60d3f11a906e
Ebuube <onwutaebubegideon1555@gmail.com>
2024-01-08 13:32:06 +0000

    Task  8: Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  // Return a string of all the set values that start with a specific string
+  if (!(set instanceof Set) || (typeof startString !== 'string') || startString === '') {
+    return '';
+  }
+
+  const filtered = Array.from(set)
+    .filter((element) => element.startsWith(startString))
+    .map((element) => element.replace(startString, ''))
+    .reduce((sum, element) => sum.concat('-', element));
+  return filtered;
+}
