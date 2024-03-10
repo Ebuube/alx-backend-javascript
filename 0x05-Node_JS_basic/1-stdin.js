970d39d3b9461a1a498d3c6b840ae5b7da4c86e5
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-10 16:47:44 +0000

    Task 1: using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,17 @@
 /* eslint-disable prefer-arrow-callback */
-let inputData = '';
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', function myConsole(data) {
+process.stdin.on('readable', () => {
   // Process input
-  const input = data.toString().trim();
-  inputData = input;
+  const data = process.stdin.read();
 
-  // Output result to stdout'
-  process.stdout.write(`Your names is: ${input}\n`);
+  if (data) {
+    // Output result to stdout'
+    process.stdout.write(`Your names is: ${data}`);
+  }
 });
 
-process.stdin.on('close', function display() {
-  // process.stdout.write(`{ inputData: ${inputData}, inputData.length: ${inputData.length} }`);
-  if (inputData.length === 0) {
-    // Do this when CTRL-D is triggered
-  } else {
-    // Do this on EOF from other commands
-    process.stdout.write('This important software is now closing\n');
-  }
+process.stdin.on('end', function display() {
+  // Do this on EOF from other commands
+  process.stdout.write('This important software is now closing\n');
 });
