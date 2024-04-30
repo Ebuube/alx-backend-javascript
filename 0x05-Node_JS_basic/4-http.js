bd893abc56fa204b8fb8dfe999d0f8528a39c622
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-30 13:18:43 +0000

    Removed wronge response format

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,16 @@
 const http = require('http');
 
-const port = process.env.PORT || 1245;
-const host = 'localhost';
+const port = 1245;
 const app = http.createServer();
 
 // Listen to the request event
 app.on('request', (request, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, host, () => {
-  process.stdout.write(`Server is listening at http://${host}:${port}/\n`);
+app.listen(port, () => {
+  // process.stdout.write(`Server is listening at http://${host}:${port}/\n`);
 });
 
 module.exports = app;
