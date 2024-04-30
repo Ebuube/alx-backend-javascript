7e191abdffff252c093af511ff006845b2a56311
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-30 13:11:31 +0000

    Using http module differently

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,17 @@
-const app = require('http');
+const http = require('http');
 
 const port = process.env.PORT || 1245;
+const host = 'localhost';
+const app = http.createServer();
 
-app.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(port, () => {
-  /* Info to log on terminal */
+// Listen to the request event
+app.on('request', (request, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {
+  process.stdout.write(`Server is listening at http://${host}:${port}/\n`);
 });
 
 module.exports = app;
