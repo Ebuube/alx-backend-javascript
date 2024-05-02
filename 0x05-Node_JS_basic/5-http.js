eb3e97250b0dd419ef1b6bb75240c97d6c4ba65d
Ebuube <onwutaebubegideon1555@gmail.com>
2024-05-02 13:22:27 +0000

    Task 5: updated

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const url = require('url');
+const { URL } = require('url');
 const countStudents = require('./utility');
 
 const port = process.env.PORT || 1245;
@@ -11,19 +11,19 @@ const dbName = process.argv[2] || 'database.csv';
 app.on('request', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   // Parse the request url
-  const reqUrl = url.parse(req.url).pathname;
+  const reqUrl = new URL(req.url, `http://${req.headers.host}`);
   // Compare our request method
-  if (req.method === 'GET' && reqUrl === '/') {
+  if (req.method === 'GET' && reqUrl.pathname === '/') {
     res.statusCode = 200;
     res.end('Hello Holberton School!');
-  } else if (req.method === 'GET' && reqUrl === '/students') {
+  } else if (req.method === 'GET' && reqUrl.pathname === '/students') {
     res.statusCode = 200;
     countStudents(dbName).then((payload) => {
       res.end(Buffer.from(payload));
     })
       .catch((err) => {
         res.statusCode = 500;
-        res.end(Buffer.from(JSON.stringify(err)));
+        res.end(JSON.stringify(err));
       });
   } else {
     res.statusCode = 404;
