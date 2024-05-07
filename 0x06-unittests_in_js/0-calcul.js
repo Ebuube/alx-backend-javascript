430da021d6545d6517b0e022b45b7c63650201a0
Ebuube <onwutaebubegideon1555@gmail.com>
2024-05-07 10:04:28 +0000

    Working tests

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,11 @@
 /**
- * Calculate the sum of two integers.
+ * Calculate the sum of rounded values of two integers.
  * @param {number} a The first integer.
  * @param {number} b The second integer
  * @returns {number} The sum of a and b.
  */
 function calculateNumber(a, b) {
-  return a + b;
+  return Math.round(a) + Math.round(b);
 }
+
+module.exports = calculateNumber;
