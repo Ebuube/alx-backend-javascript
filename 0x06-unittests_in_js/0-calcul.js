a6a5160df04a7b37a913eb7cfbd1a37394d1c8c2
Ebuube <onwutaebubegideon1555@gmail.com>
2024-05-07 09:28:24 +0000

    startup

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+/**
+ * Calculate the sum of two integers.
+ * @param {number} a The first integer.
+ * @param {number} b The second integer
+ * @returns {number} The sum of a and b.
+ */
+function calculateNumber(a, b) {
+  return a + b;
+}
