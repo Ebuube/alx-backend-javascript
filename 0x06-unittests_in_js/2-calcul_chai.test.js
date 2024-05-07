08b1c4db1f72f0a977d6c7ddc35cc50ed739a6fe
Ebuube <onwutaebubegideon1555@gmail.com>
2024-05-07 11:45:49 +0000

    Added all test suites

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,51 +1,42 @@
-// const assert = require("assert");
 const chai = require('chai');
 const expect = chai.expect;
-const calculateNumber = require("./2-calcul");
+const calculateNumber = require('./2-calcul_chai');
 
-describe("Test for calculateNumber", function() {
-  it("SUM: two integers", function() {
-    expect(calculateNumber('SUM', 1, 3)).timedOut.equal(4);
-  })
-});
-
-/**
-describe("Test for calculateNumber", function() {
-  it("SUM: two integers", function() {
-    assert.equal(calculateNumber('SUM', 1, 3), 4);
+describe('Test for calculateNumber', function() {
+  it('SUM: two integers', function() {
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
   });
-  it("SUM: two floats", function() {
-    assert.equal(calculateNumber('SUM', 1.3, 3.7), 5);
+  it('SUM: two floats', function() {
+    expect(calculateNumber('SUM', 1.3, 3.7)).to.equal(5);
   });
-  it("SUM: negative integers", function() {
-    assert.equal(calculateNumber('SUM', -1.5, -3.7), -5);
+  it('SUM: negative integers', function() {
+    expect(calculateNumber('SUM', -1.5, -3.7)).to.equal(-5);
   });
 
-  it("SUBTRACT: two integers", function() {
-    assert.equal(calculateNumber('SUBTRACT', 1, 3), -2);
+  it('SUBTRACT: two integers', function() {
+    expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
   });
-  it("SUBTRACT: two floats", function() {
-    assert.equal(calculateNumber('SUBTRACT', 1.3, 3.7), -3);
+  it('SUBTRACT: two floats', function() {
+    expect(calculateNumber('SUBTRACT', 1.3, 3.7)).to.equal(-3);
   });
-  it("SUBTRACT: negative integers", function() {
-    assert.equal(calculateNumber('SUBTRACT', -1.5, -3.7), 3);
-  });
-
-  it("DIVIDE: two integers", function() {
-    assert.equal(calculateNumber('DIVIDE', 1, 3), 1 / 3);
-  });
-  it("DIVIDE: two floats", function() {
-    assert.equal(calculateNumber('DIVIDE', 1.3, 3.7), 0.25);
-  });
-  it("DIVIDE: negative integers", function() {
-    assert.equal(calculateNumber('DIVIDE', -1.5, -3.7), 0.25);
+  it('SUBTRACT: negative integers', function() {
+    expect(calculateNumber('SUBTRACT', -1.5, -3.7)).to.equal(3);
   });
 
-  it("DIVIDE: (integers) zerodivision error", function() {
-    assert.equal(calculateNumber('DIVIDE', 4, 0), 'Error');
+  it('DIVIDE: two integers', function() {
+    expect(calculateNumber('DIVIDE', 1, 3)).to.equal(1 / 3);
   });
-  it("DIVIDE: (floats) zerodivision error", function() {
-    assert.equal(calculateNumber('DIVIDE', 3.5, 0.3), 'Error');
+  it('DIVIDE: two floats', function() {
+    expect(calculateNumber('DIVIDE', 1.3, 3.7)).to.equal(0.25);
+  });
+  it('DIVIDE: negative integers', function() {
+    expect(calculateNumber('DIVIDE', -1.5, -3.7)).to.equal(0.25);
+  });
+
+  it('DIVIDE: (integers) zerodivision error', function() {
+    expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
+  });
+  it('DIVIDE: (floats) zerodivision error', function() {
+    expect(calculateNumber('DIVIDE', 3.5, 0.3)).to.equal('Error');
   });
 });
-*/
